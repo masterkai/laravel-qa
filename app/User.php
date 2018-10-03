4de46055525ac133c575a0b8fe822486e404dbb6
masterkai <kai0919@hotmail.com>
2018-10-03 22:28:57 +0800

    Generate Question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -6,25 +6,38 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Authenticatable {
+	use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
+
+	public function questions()
+	{
+		return $this->hasMany(Question::class);
+	}
+
+	public function setTitleAttribute($value)
+	{
+		$this->attributes['title']= $value;
+		$this->attributes['slug']= str_slug($value);
+	}
 }
